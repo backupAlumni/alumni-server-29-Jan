81fc79af01ac29f6f36ddc67c29339890b3e1261
Tran203 <tshetshe203@gmail.com>
2023-09-08 11:56:30 +0200

    update script

--- database/q'phela.sql
+++ database/q'phela.sql
@@ -9,60 +9,26 @@ CREATE TABLE ALUMNI_SPACE_UI (
 
 CREATE TABLE Alumni_Space_Account (
     account_id SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE, -- Added UNIQUE constraint
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
-    role VARCHAR(20) NOT NULL,
+    role VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Tut_Alumni (
     alumni_id SERIAL PRIMARY KEY,
     account_id INT REFERENCES Alumni_Space_Account(account_id),
     name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
-    --date_of_birth DATE,
+    surname VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Administrator (
     admin_id SERIAL PRIMARY KEY,
     account_id INT REFERENCES Alumni_Space_Account(account_id),
     name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL
 );
 
-<<<<<<< Updated upstream
-Create DATABASE Alumni_Space_DB;
 
-CREATE TABLE ALUMNI_SPACE_UI (
-    account_id SERIAL PRIMARY KEY,
-    fullname VARCHAR(20) NOT NULL
-    email VARCHAR(50) NOT NULL UNIQUE, -- Added UNIQUE constraint
-    password VARCHAR(100) NOT NULL,
-);
-
-CREATE TABLE Alumni_Space_Account (
-    account_id SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE, -- Added UNIQUE constraint
-    password VARCHAR(100) NOT NULL,
-    role VARCHAR(20) NOT NULL,
-);
-
-CREATE TABLE Tut_Alumni (
-    alumni_id SERIAL PRIMARY KEY,
-    account_id INT REFERENCES Alumni_Space_Account(account_id),
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
-    date_of_birth DATE,
-);
-
-CREATE TABLE Administrator (
-    admin_id SERIAL PRIMARY KEY,
-    account_id INT REFERENCES Alumni_Space_Account(account_id),
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
-);
-
-=======
->>>>>>> Stashed changes
 --Profile
 
 --Profile
