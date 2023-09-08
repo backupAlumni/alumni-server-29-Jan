388b5bea96ac56559c1a6b32f82cf0c5993f5ff8
Kfefe23 <136083032+Kfefe23@users.noreply.github.com>
2023-09-08 09:19:22 +0200

    Update q'phela.sql

--- database/q'phela.sql
+++ database/q'phela.sql
@@ -9,25 +9,25 @@ CREATE TABLE ALUMNI_SPACE_UI (
 
 CREATE TABLE Alumni_Space_Account (
     account_id SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE, -- Added UNIQUE constraint
     password VARCHAR(100) NOT NULL,
-    role VARCHAR(20) NOT NULL
+    role VARCHAR(20) NOT NULL,
 );
 
 CREATE TABLE Tut_Alumni (
     alumni_id SERIAL PRIMARY KEY,
     account_id INT REFERENCES Alumni_Space_Account(account_id),
     name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL
+    surname VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
 );
 
 CREATE TABLE Administrator (
     admin_id SERIAL PRIMARY KEY,
     account_id INT REFERENCES Alumni_Space_Account(account_id),
     name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL
+    surname VARCHAR(50) NOT NULL,
 );
-
 
 --Profile
 
