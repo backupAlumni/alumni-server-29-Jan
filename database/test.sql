7d5cd5ea146fa3d5ef288d82bd98cf34ece159c7
Kfefe23 <kefentsemnisi200@gmail.com>
2023-11-03 13:49:20 +0200

    updated sql

--- database/test.sql
+++ database/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2023 at 04:15 PM
+-- Generation Time: Nov 03, 2023 at 12:44 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -65,6 +65,20 @@ INSERT INTO `alumni_space_account` (`account_id`, `email`, `password`, `role`) V
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `event_id` bigint(20) UNSIGNED NOT NULL,
+  `alumni_id` int(11) DEFAULT NULL,
+  `event_title` varchar(50) NOT NULL,
+  `event_description` varchar(100) NOT NULL,
+  `event_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `joblisting`
 --
 
@@ -77,17 +91,9 @@ CREATE TABLE `joblisting` (
   `location` varchar(50) NOT NULL,
   `job_type` varchar(50) NOT NULL,
   `job_description` varchar(50) NOT NULL,
-  `date_posted` date DEFAULT NULL,
-  `deadline` date DEFAULT NULL
+  `date_posted` datetime DEFAULT NULL,
+  `deadline` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `joblisting`
---
-
-INSERT INTO `joblisting` (`job_id`, `account_id`, `Organisation`, `job_title`, `workplace_type`, `location`, `job_type`, `job_description`, `date_posted`, `deadline`) VALUES
-(1, NULL, 'AWS', 'Developer', 'Hybrid', 'Emalahleni', 'Full-time', 'zxzdzdfg', '2023-10-26', '2023-10-10'),
-(2, NULL, 'sad', 'sad', 'Hybrid', 'Emalahleni', 'Full-time', 'sadsa', '2023-10-25', '2023-10-10');
 
 -- --------------------------------------------------------
 
@@ -107,7 +113,16 @@ CREATE TABLE `tut_alumni` (
 --
 
 INSERT INTO `tut_alumni` (`alumni_id`, `account_id`, `name`, `surname`) VALUES
-(1, 2, 'Lindo', 'Zane');
+(1, 2, 'Lindo', 'Zane'),
+(2, 3, 'Sihle', 'Mlongo'),
+(3, 7, 'Themba', 'Mabena'),
+(4, 5, 'Kefentse', 'Mnisi'),
+(5, 6, 'Snenhlanhla', 'Masuku'),
+(6, 4, 'Innocent', 'Masia'),
+(7, 8, 'Noxolo', 'Sibiya'),
+(8, 3, 'Elias', 'Mashia'),
+(9, 3, 'Kgaogelo', 'Lebelo'),
+(10, 3, 'Kabelo', 'Lehlojane');
 
 -- --------------------------------------------------------
 
@@ -132,7 +147,11 @@ CREATE TABLE `userprofile` (
 --
 
 INSERT INTO `userprofile` (`user_id`, `account_id`, `location`, `qualification`, `employment_status`, `skills`, `experience`, `interest`, `bio`) VALUES
-(1, 2, '', '', '', '', '', '', '');
+(1, 2, '', '', '', '', '', '', ''),
+(2, 1, 'Soshanguve', 'PHD', 'Employed', 'Java,React,Angular,NodeJS,MySQL Database', '6 Years', 'Coding/Full-Stack', 'team player and hackerthon master'),
+(3, 4, 'Hammanskraal', 'Doctrate', 'Employed', 'C#,Java,React,Angular,NodeJS,MySQL Database', '12 Years', 'Full-Stack', ' hackerthon master'),
+(4, 5, 'Kwazulu-Natal', 'Diploma', 'UnEmployed', 'Java,MySQL Database', '0', 'Business Analyst', 'like attending hackerthon'),
+(5, 6, 'Sandton', 'masters', 'Self-Employed', 'Java,AWS Deploying,MySQL Database,Azure', '2 months', 'Scrum Master', ' hackerthon Master');
 
 --
 -- Indexes for dumped tables
@@ -150,6 +169,12 @@ ALTER TABLE `administrator`
 ALTER TABLE `alumni_space_account`
   ADD PRIMARY KEY (`account_id`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`);
 
 --
 -- Indexes for table `joblisting`
@@ -186,22 +211,28 @@ ALTER TABLE `alumni_space_account`
   MODIFY `account_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `joblisting`
 --
 ALTER TABLE `joblisting`
-  MODIFY `job_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `job_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tut_alumni`
 --
 ALTER TABLE `tut_alumni`
-  MODIFY `alumni_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `alumni_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `userprofile`
 --
 ALTER TABLE `userprofile`
-  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
